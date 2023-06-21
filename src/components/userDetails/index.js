0c940fbad37e9960c7e9e92a3fd862a66b1306a8
vijaykumardumpeti <vijaydumpeti70@gmail.com>
2023-06-21 14:20:39 +0000

    successfully fetched data and stored in redux store and subscribed that store to accessed that state and dispatch method and used in the component UI

---
+++ src/components/userDetails/index.js
@@ -0,0 +1,39 @@
+import { fetchUsers } from "../../redux/user/userActions";
+
+import { connect } from "react-redux";
+
+import { useEffect } from "react";
+import "./index.css";
+
+const UserDetails = (props) => {
+  const { fetchUsers, userDetails } = props;
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
+
+  return (
+    <div className="container">
+      {userDetails.users.map((eachUser) => (
+        <div key={eachUser.id} className="item">
+          <h1>{eachUser.name}</h1>
+          <h2>{eachUser.username}</h2>
+          <h3>{eachUser.email}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    userDetails: state.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUsers: () => dispatch(fetchUsers()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserDetails);
